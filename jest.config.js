7434488ee97a31c2b0eed57542067b78d7e21f7f
Taise soares <contato@taisesoares.com>
2022-03-24 08:52:31 -0300

    feat: config coverage
    Signed-off-by: Taise soares <contato@taisesoares.com>

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
+  collectCoverageFrom: ['<rootDir>/components/**/*.js', '<rootDir>/pages/**/*.js'],
   testEnvironment: 'jsdom'
 }
